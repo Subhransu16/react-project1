aca19809d53da9d94368d48c69d3a9290a8e9c31
Subhransu16 <subhransuparida35@gmail.com>
2025-08-29 17:51:35 +0530

    react-project

---
+++ project1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HeroSection from './Hero';
+import Navigation from './navigation';
+import React from 'react';
+
+const App = () => {
+
+    return <div>
+     <Navigation />
+     <HeroSection/>
+    </div>;
+};
+
+
+export default App;
